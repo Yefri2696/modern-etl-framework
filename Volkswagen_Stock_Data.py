5552d2d840f915a0d5d271bbfe6591410eba10db
Oscar Hernandez <148633267+Yefri2696@users.noreply.github.com>
2026-06-26 22:23:54 +0200

    Enhance Volkswagen stock data processing pipeline
    Updated the Volkswagen stock data pipeline to include quality checks and export the cleaned dataset to a CSV file.

--- Volkswagen_Stock_Data.py
+++ Volkswagen_Stock_Data.py
@@ -1,13 +1,23 @@
-import matplotlib.pyplot as plt
+"""Pipeline: Volkswagen Stock Data (1995–2026)
+Author: Oscar Hernández
+Goal:
+- Extract historical Volkswagen stock data
+- Clean, normalize, and convert data types
+- Prepare dataset for financial analysis and dashboards
+
+Flow:
+Extract → Clean → TypeCast → Export"""
+
 import pandas as pd
-
-
 from extract import  extract
 from transform import TransformPipeline
 from transform import ColumnCleaner
 from transform import TypeCaster
 
 
+#==========================================
+#1. EXTRACT
+#==========================================
 
 df1 = extract(r'C:\Users\yefri\OneDrive\Escritorio\datos\Data\Dataset\Volkswagen Stock Data (1995-2026).zip')
 
@@ -17,10 +27,16 @@ print(df1.columns)
 print(df1.dtypes)
 print(df1.head(10))
 
+#=========================================
+#2. QUALITY CHECKS
+#=========================================
+
 print(df1.duplicated().sum())
 print('********************\n',df1.duplicated('BB_Upper').sum())
 
-
+#==========================================
+#3. TRANSFORM PIPELINE
+#==========================================
 pipeline = TransformPipeline([ColumnCleaner(strip=True, lower=True, null_strategy= 0, drop_duplicates=True),
                               TypeCaster({'date': 'auto', 'year': 'datetime', 'month': 'auto', 'low': 'auto'})])
 
@@ -28,14 +44,10 @@ df= pipeline.run(df1)
 print(df.dtypes)
 print(df.head(10))
 
-
-
-'''pipeline = TransformPipeline([
-    ColumnCleaner(strip=True, lower=True),
-    TypeCaster({"id": int, "cholesterol": int})  # opcional
-])'''
-
-df.to_csv('Volkswagen Stock Data (1995-2026).csv', index=False)
+#========================================
+#4. EXPORT
+#=======================================
+df.to_csv('Volkswagen Stock Data (1995-2026) clean.csv', index=False)
 
 
 
